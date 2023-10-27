f523998683f0b2e44625ca3aef6aa249007e37cf
KhaledAbuSarhan <khaleded888@gmail.com>
2023-10-27 23:02:22 +0300

    changing api url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         SetGeneratingImg(true);
-        const response = await fetch(`http://localhost:8081/api/v1/dalle`, {
+        const response = await fetch(`https://text-to-image-ai-kzg6.onrender.com/api/v1/dalle`, {
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json'
@@ -55,7 +55,7 @@ const CreatePost = () => {
     if(form?.prompt && form?.photo) {
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:8081/api/v1/post', {
+        const response = await fetch('https://text-to-image-ai-kzg6.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json'
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         const fetchPosts = async () => {
             setLoading(true)
             try {
-                const response = await fetch('http://localhost:8081/api/v1/post', {
+                const response = await fetch('https://text-to-image-ai-kzg6.onrender.com/api/v1/post', {
                     method: 'GET',
                     headers : {
                         'Content-Type' : 'application/json',
